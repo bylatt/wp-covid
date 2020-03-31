4338af043f34462858d66c4767b25b63b94c033b
Lattapon Yodsuwan <clozed2u@gmail.com>
2020-03-31 23:08:22 +0700

    Add try..catch block

--- bin/covid.js
+++ bin/covid.js
@@ -5,25 +5,29 @@ const colors = require("colors/safe");
 const argv = require("yargs").argv;
 const getStat = require("../");
 const cli = async country => {
-  let table = new Table({
-    head: [
-      colors.blue("Country"),
-      colors.yellow("Confirmed"),
-      colors.green("Recovered"),
-      colors.red("Deaths")
-    ]
-  });
-  if (country) {
-    const stat = (await getStat(country))[0];
-    table.push([stat.name, stat.confirmed, stat.recovered, stat.deaths]);
-  } else {
-    let stats = await getStat();
-    stats.sort((a, b) => (a.confirmed < b.confirmed ? 1 : -1));
-    for (let stat of stats) {
+  try {
+    let table = new Table({
+      head: [
+        colors.blue("Country"),
+        colors.yellow("Confirmed"),
+        colors.green("Recovered"),
+        colors.red("Deaths")
+      ]
+    });
+    if (country) {
+      const stat = (await getStat(country))[0];
       table.push([stat.name, stat.confirmed, stat.recovered, stat.deaths]);
+    } else {
+      let stats = await getStat();
+      stats.sort((a, b) => (a.confirmed < b.confirmed ? 1 : -1));
+      for (let stat of stats) {
+        table.push([stat.name, stat.confirmed, stat.recovered, stat.deaths]);
+      }
     }
+    console.log(table.toString());
+  } catch (e) {
+    console.log(colors.red("failed to get stat(s)"));
   }
-  console.log(table.toString());
 };
 const country = argv.country;
 cli(country);
